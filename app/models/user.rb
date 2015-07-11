f4c30adda64f40fcb1dfa3952d98c3107e34a86b
Vladislav Faust <vladislav.faust@gmail.com>
2015-07-12 01:31:11 +0300

    Added User model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+
+  # email
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+end
