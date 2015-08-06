3699fce627aec80d679ba7c57983d100501d68f9
Vladislav Faust <vladislav.faust@gmail.com>
2015-08-07 00:43:14 +0300

    Some exercises. Code improved

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation: 'bar'  }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger.alert'
   end
 
   test 'valid signup information' do
@@ -24,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
 
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
