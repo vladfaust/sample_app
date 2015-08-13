ea44dd3da461339e2e2174c00ca939e2c86a595c
Vladislav Faust <vladislav.faust@gmail.com>
2015-08-13 15:55:59 +0300

    Delete users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-User.create!( name: 'Example user',
-              email: 'example@railstutorial.org',
-              password: 'foobar',
-              password_confirmation: 'foobar' )
+User.create!( name:                   'Example User',
+              email:                  'example@railstutorial.org',
+              password:               'foobar',
+              password_confirmation:  'foobar',
+              admin:                  true)
 
 99.times do |n|
   name = Faker::Name.name
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,4 +39,19 @@ class UsersControllerTest < ActionController::TestCase
     get :index
     assert_redirected_to login_url
   end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect destroy when not admin' do
+    log_in_as(@another_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
 end
