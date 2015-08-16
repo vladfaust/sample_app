7ea85cc1c5a76ef82109841777d498f93cb89c8f
Vladislav Faust <vladislav.faust@gmail.com>
2015-08-17 01:13:39 +0300

    Uploading images

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ gem 'faker',                    '1.4.2'
 gem 'will_paginate',            '3.0.7'
 gem 'bootstrap-will_paginate',  '0.0.10'
 
+gem 'carrierwave',             '0.10.0'
+gem 'mini_magick',             '3.8.0'
+gem 'fog',                     '1.23.0'
+
 group :development, :test do
 
   # Use sqlite3 as the database for Active Record
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,14 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+
+  private
+
+  # Validates the size of uploaded picture
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, 'should be less than 5MB')
+    end
+  end
 end
