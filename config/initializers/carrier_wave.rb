1d10010a05c30b505d6ada4d3a06d2c7bd7203a5
Vladislav Faust <vladislav.faust@gmail.com>
2015-08-17 01:26:40 +0300

    Small uploading issue fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,6 @@ if Rails.env.production?
         :google_storage_access_key_id     => ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
         :google_storage_secret_access_key => ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
     }
-    config.fog_directory     =  'pictures'
+    config.fog_directory     =  'sample_app_pictures'
   end
 end
