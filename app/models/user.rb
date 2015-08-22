4fb40ec04b0d1664e85c95b17549570062fd2bc2
Vlad <vladislav.faust@gmail.com>
2015-08-22 13:14:19 +0300

    Relationships base; user's followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
 
+  # Relationshit
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
   before_create :create_activation_digest
 
   # Name
@@ -81,6 +85,23 @@ class User < ActiveRecord::Base
   # Defines a proto-feed
   def feed
 	  Micropost.where('user_id = ?', id)
+  end
+
+  # Followings
+
+  # Follows a user
+  def follow (other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user
+  def unfollow (other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Retutns if this user follows another one
+  def following? (other_user)
+    following.include?(other_user)
   end
 
   private
