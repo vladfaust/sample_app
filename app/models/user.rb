130c023f105383389f9e00ba89eef86e8e04b482
Vlad <vladislav.faust@gmail.com>
2015-08-22 13:16:33 +0300

    User's followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   # Relationshit
   has_many :active_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy
+  has_many :followers, through: :passive_relationships
 
   before_create :create_activation_digest
 
